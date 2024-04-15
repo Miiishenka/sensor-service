abfe3b8b579ac3ca5095244fce629513249f35cb
CHelebaev_Mikhail <367611@niutmo.ru>
2024-04-15 22:38:13 +0300

    Made HW8

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,15 @@
 package main
 
+//go:generate swagger generate model -f ../../api/swagger.yaml -t ../../internal/
+
 import (
 	"errors"
+	"fmt"
 	"homework/internal/usecase"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	httpGateway "homework/internal/gateways/http"
 	eventRepository "homework/internal/repository/event/inmemory"
@@ -24,9 +29,19 @@ func main() {
 		User:   usecase.NewUser(ur, sor, sr),
 	}
 
-	// TODO реализовать веб-сервис
+	httpHost := os.Getenv("HTTP_HOST")
+	httpPort, err := strconv.ParseUint(os.Getenv("HTTP_PORT"), 10, 16)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "HTTP_PORT should be unsigned 16-bit integer")
+		os.Exit(1)
+	}
 
-	r := httpGateway.NewServer(useCases)
+	r := httpGateway.NewServer(
+		useCases,
+		httpGateway.WithHost(httpHost),
+		httpGateway.WithPort(uint16(httpPort)),
+	)
+
 	if err := r.Run(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Printf("error during server shutdown: %v", err)
 	}
--- internal/gateways/http/router.go
+++ internal/gateways/http/router.go
@@ -1,9 +1,302 @@
 package http
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"homework/internal/domain"
+	"homework/internal/models"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
 
-func setupRouter(r *gin.Engine, _ UseCases) {
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter(r *gin.Engine, uc UseCases) {
+	r.HandleMethodNotAllowed = true
+
+	r.POST("/users", func(c *gin.Context) {
+		if c.ContentType() != "application/json" {
+			c.AbortWithStatus(http.StatusUnsupportedMediaType)
+			return
+		}
+
+		userDto := &models.UserToCreate{}
+		if err := c.BindJSON(userDto); err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		if err := userDto.Validate(nil); err != nil {
+			c.AbortWithStatus(http.StatusUnprocessableEntity)
+			return
+		}
+
+		user := &domain.User{Name: *userDto.Name}
+		user, err := uc.User.RegisterUser(c, user)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, err)
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
+	})
+
+	r.OPTIONS("/users", func(c *gin.Context) {
+		methods := []string{http.MethodOptions, http.MethodPost}
+		c.Header("Allow", strings.Join(methods, ","))
+		c.Status(http.StatusNoContent)
+	})
+
+	r.GET("/sensors", func(c *gin.Context) {
+		if c.GetHeader("Accept") != "application/json" {
+			c.AbortWithStatus(http.StatusNotAcceptable)
+			return
+		}
+
+		sensors, err := uc.Sensor.GetSensors(c)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, err)
+		}
+		c.JSON(http.StatusOK, sensors)
+	})
+
+	r.HEAD("/sensors", func(c *gin.Context) {
+		if c.GetHeader("Accept") != "application/json" {
+			c.AbortWithStatus(http.StatusNotAcceptable)
+			return
+		}
+
+		sensors, err := uc.Sensor.GetSensors(c)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, err)
+		}
+		jsonSensor, _ := json.Marshal(sensors)
+		c.Header("Content-Length", strconv.Itoa(len(jsonSensor)))
+		c.Status(http.StatusOK)
+	})
+
+	r.POST("/sensors", func(c *gin.Context) {
+		if c.ContentType() != "application/json" {
+			c.AbortWithStatus(http.StatusUnsupportedMediaType)
+			return
+		}
+
+		sensorDto := &models.SensorToCreate{}
+		if err := c.BindJSON(sensorDto); err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		if err := sensorDto.Validate(nil); err != nil {
+			c.AbortWithStatus(http.StatusUnprocessableEntity)
+			return
+		}
+
+		sensor := &domain.Sensor{
+			Description:  *sensorDto.Description,
+			IsActive:     *sensorDto.IsActive,
+			SerialNumber: *sensorDto.SerialNumber,
+			Type:         domain.SensorType(*sensorDto.Type),
+		}
+		sensor, err := uc.Sensor.RegisterSensor(c, sensor)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, err)
+			return
+		}
+
+		c.JSON(http.StatusOK, sensor)
+	})
+
+	r.OPTIONS("/sensors", func(c *gin.Context) {
+		methods := []string{
+			http.MethodOptions,
+			http.MethodPost,
+			http.MethodGet,
+			http.MethodHead,
+		}
+
+		c.Header("Allow", strings.Join(methods, ","))
+		c.Status(http.StatusNoContent)
+	})
+
+	r.GET("/sensors/:id", func(c *gin.Context) {
+		id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnprocessableEntity, err)
+			return
+		}
+		sensor, err := uc.Sensor.GetSensorByID(c, id)
+		if err != nil {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		if c.GetHeader("Accept") != "application/json" {
+			c.AbortWithStatus(http.StatusNotAcceptable)
+			return
+		}
+
+		c.JSON(http.StatusOK, sensor)
+	})
+
+	r.HEAD("/sensors/:id", func(c *gin.Context) {
+		id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		sensor, err := uc.Sensor.GetSensorByID(c, id)
+		if err != nil {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		if c.GetHeader("Accept") != "application/json" {
+			c.AbortWithStatus(http.StatusNotAcceptable)
+			return
+		}
+
+		jsonSensor, _ := json.Marshal(sensor)
+		c.Header("Content-Length", strconv.Itoa(len(jsonSensor)))
+		c.Status(http.StatusOK)
+	})
+
+	r.OPTIONS("/sensors/:id", func(c *gin.Context) {
+		methods := []string{
+			http.MethodOptions,
+			http.MethodGet,
+			http.MethodHead,
+		}
+
+		c.Header("Allow", strings.Join(methods, ","))
+		c.Status(http.StatusNoContent)
+	})
+
+	r.GET("/users/:id/sensors", func(c *gin.Context) {
+		id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		sensors, err := uc.User.GetUserSensors(c, id)
+		if err != nil {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		if c.GetHeader("Accept") != "application/json" {
+			c.AbortWithStatus(http.StatusNotAcceptable)
+			return
+		}
+
+		c.JSON(http.StatusOK, sensors)
+	})
+
+	r.HEAD("/users/:id/sensors", func(c *gin.Context) {
+		id, err := strconv.ParseInt(c.Param("id"), 10, 64)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		sensors, err := uc.User.GetUserSensors(c, id)
+		if err != nil {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		if c.GetHeader("Accept") != "application/json" {
+			c.AbortWithStatus(http.StatusNotAcceptable)
+			return
+		}
+
+		jsonSensors, _ := json.Marshal(sensors)
+		c.Header("Content-Length", strconv.Itoa(len(jsonSensors)))
+		c.Status(http.StatusOK)
+	})
+
+	r.POST("/users/:id/sensors", func(c *gin.Context) {
+		userId, err := strconv.ParseInt(c.Param("id"), 10, 64)
+		if err != nil {
+			c.AbortWithStatus(http.StatusUnprocessableEntity)
+			return
+		}
+
+		if c.ContentType() != "application/json" {
+			c.AbortWithStatus(http.StatusUnsupportedMediaType)
+			return
+		}
+
+		sensorDto := &models.SensorToUserBinding{}
+		if err := c.BindJSON(sensorDto); err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		if err := sensorDto.Validate(nil); err != nil {
+			c.AbortWithStatus(http.StatusUnprocessableEntity)
+			return
+		}
+
+		err = uc.User.AttachSensorToUser(c, userId, *sensorDto.SensorID)
+		if err != nil {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		c.Status(http.StatusCreated)
+	})
+
+	r.OPTIONS("/users/:id/sensors", func(c *gin.Context) {
+		methods := []string{
+			http.MethodOptions,
+			http.MethodPost,
+			http.MethodHead,
+			http.MethodGet,
+		}
+
+		c.Header("Allow", strings.Join(methods, ","))
+		c.Status(http.StatusNoContent)
+	})
+
+	r.POST("/events", func(c *gin.Context) {
+		if c.ContentType() != "application/json" {
+			c.AbortWithStatus(http.StatusUnsupportedMediaType)
+			return
+		}
+
+		eventDto := &models.SensorEvent{}
+		if err := c.BindJSON(eventDto); err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+
+		if err := eventDto.Validate(nil); err != nil {
+			c.AbortWithStatus(http.StatusUnprocessableEntity)
+			return
+		}
+
+		event := &domain.Event{
+			Payload:            *eventDto.Payload,
+			SensorSerialNumber: *eventDto.SensorSerialNumber,
+			Timestamp:          time.Now(),
+		}
+
+		err := uc.Event.ReceiveEvent(c, event)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, err)
+			return
+		}
+
+		c.Status(http.StatusCreated)
+	})
+
+	r.OPTIONS("/events", func(c *gin.Context) {
+		methods := []string{http.MethodOptions, http.MethodPost}
+		c.Header("Allow", strings.Join(methods, ","))
+		c.Status(http.StatusNoContent)
 	})
 }
